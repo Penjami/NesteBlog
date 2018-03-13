28c15cff1f452a1304d56ab4afed299c79388a2f
Penjami <penjami.rantakangas@cs.tamk.fi>
2018-03-13 09:59:20 +0200

    Folder initialization.

---
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
@@ -0,0 +1,12 @@
+package fi.tamk.tiko.neste.nesteblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NesteblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NesteblogApplication.class, args);
+	}
+}
