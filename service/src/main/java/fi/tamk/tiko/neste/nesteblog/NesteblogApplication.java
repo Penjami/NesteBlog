cdbf8481ce13a56f72b12e01695562eae3ede211
Infinitz <ristopulkkinen94@gmail.com>
2018-03-13 10:24:30 +0200

    Modify test print message

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
@@ -8,6 +8,6 @@ public class NesteblogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NesteblogApplication.class, args);
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 }
