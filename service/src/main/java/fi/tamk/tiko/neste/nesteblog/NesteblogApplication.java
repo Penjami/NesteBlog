11e0e7fbca624b732cea77bc2e2cc96ba6fcbfcc
Penjami <penjami.rantakangas@cs.tamk.fi>
2018-03-18 12:34:46 +0200

    Create frontend with create-react-app, combine front and back with some tedious resource juggling.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
@@ -2,6 +2,10 @@ package fi.tamk.tiko.neste.nesteblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class NesteblogApplication {
@@ -10,4 +14,19 @@ public class NesteblogApplication {
         SpringApplication.run(NesteblogApplication.class, args);
         System.out.println("Hello world!");
     }
+
+    @Bean
+    public ClassLoaderTemplateResolver reactAppTemplateResolver() {
+        ClassLoaderTemplateResolver reactAppTemplateResolver = new ClassLoaderTemplateResolver();
+        reactAppTemplateResolver.setPrefix("/static/app/build/");
+        reactAppTemplateResolver.setSuffix(".html");
+        reactAppTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        reactAppTemplateResolver.setCharacterEncoding("UTF-8");
+        reactAppTemplateResolver.setOrder(0);
+        reactAppTemplateResolver.setCheckExistence(true);
+
+        return reactAppTemplateResolver;
+    }
+
+
 }
