b4cf87d1d47081449335fff8ada492b3c2e550ee
Infinitz <ristopulkkinen94@gmail.com>
2018-03-20 11:35:56 +0200

    Remove unused import Create new branch for release

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogRestController.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogRestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 public class BlogRestController {
