cb4ae06716516e8ca0837c60b256c9cd645a63d2
Penjami <penjami.rantakangas@cs.tamk.fi>
2018-04-10 15:17:43 +0300

    Modifying blogposts now works.

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -11,12 +11,13 @@ class App extends Component {
     this.loadBlogpostsFromDB = this.loadBlogpostsFromDB.bind(this);
 	  this.switchToNewBlog = this.switchToNewBlog.bind(this);
     this.switchToHome = this.switchToHome.bind(this);
-	  this.handleBlogSubmit = this.handleBlogSubmit.bind(this);
+	  this.handleNewBlogSubmit = this.handleNewBlogSubmit.bind(this);
+    this.handleModifyBlogSubmit = this.handleModifyBlogSubmit.bind(this);
 	  this.renderNewBlogPost = this.renderNewBlogPost.bind(this);
 	  this.renderPage = this.renderPage.bind(this)
 	  this.state = {blogPosts: [], pageState: 'HOME',
-		  pageStates: ['HOME', 'NEWPOST', 'ABOUT'],
-      author: '', title: '', content: ''};
+		  pageStates: ['HOME', 'NEWPOST', 'MODIFYPOST', 'ABOUT'],
+      author: '', title: '', content: '', id: ''};
   }
 
   onDelete(blogPost) {
@@ -25,7 +26,8 @@ class App extends Component {
 
 
 	onModify(blogPost) {
-		this.setState({pageState: this.state.pageStates[1], author: blogPost.author, title: blogPost.title, content: blogPost.content})
+		this.setState({author: blogPost.author, title: blogPost.title, content: blogPost.content, id: blogPost.id});
+    this.switchToModifyBlog();
 	}
 
   deleteBlogPost(index) {
@@ -46,13 +48,11 @@ class App extends Component {
       return response.json();
       }).then(response => {
       this.setState({blogPosts: response});
-    }).then(this.loadBlogpostsFromDB);;
+    }).then(this.loadBlogpostsFromDB);
   }
 
-	handleBlogSubmit(event) {
+	handleNewBlogSubmit(event) {
     event.preventDefault();
-		const data = new FormData(event.target);
-		const iterator = data.entries()
 
 		fetch('/blogposts/', {
 			method: 'post',
@@ -64,23 +64,47 @@ class App extends Component {
 		})}).then((result)=> {
 		  console.log(result);
     }).then(this.switchToHome).then(()=> {
-			this.setState({ author: '', title: '', content: ''})
+			this.setState({ author: '', title: '', content: ''});
 		});
 
 	}
 
-	handleChange(e) {
+  handleModifyBlogSubmit(event) {
+    event.preventDefault();
+
+    fetch('/blogposts/' + this.state.id, {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        author: this.state.author,
+        content: this.state.content,
+        title: this.state.title
+      })}).then((result)=> {
+      console.log(result);
+    }).then(this.switchToHome).then(()=> {
+      this.setState({ author: '', title: '', content: ''});
+    });
+
+  }
+
+
+  handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
-    })
+    });
   }
 
 	switchToNewBlog() {
-		this.setState({pageState: this.state.pageStates[1]})
+		this.setState({pageState: this.state.pageStates[1]});
 	}
 
+
+  switchToModifyBlog() {
+    this.setState({pageState: this.state.pageStates[2]});
+  }
+
 	switchToHome() {
-    this.setState({pageState: this.state.pageStates[0]})
+    this.setState({pageState: this.state.pageStates[0]});
   }
 
 	renderHome() {
@@ -107,19 +131,43 @@ class App extends Component {
 					<li><a>About</a></li>
 					<li><a onClick={this.switchToNewBlog}>Create New Blog Post</a></li>
 				</ul>
-				<form onSubmit={this.handleBlogSubmit}>
+				<form onSubmit={this.handleNewBlogSubmit}>
           <p>author</p>
 					<input type='text' name='author' value={this.state.author} onChange={e => this.handleChange(e)}/>
           <p>title</p>
 					<input type='text' name='title' value={this.state.title} onChange={e => this.handleChange(e)}/>
           <p>content</p>
 					<textarea rows="4" cols="50" name='content' value={this.state.content} onChange={e => this.handleChange(e)}></textarea>
-          <button type='Submit'>save</button>
+          <button type='Submit'>Save New</button>
 				</form>
 				<footer></footer>
 			</page>
 		)
 	}
+
+
+  renderModifyBlogPost() {
+    return (
+			<page>
+				<ul>
+					<li><a onClick={this.switchToHome}>Home</a></li>
+					<li><a>Contact</a></li>
+					<li><a>About</a></li>
+					<li><a onClick={this.switchToNewBlog}>Create New Blog Post</a></li>
+				</ul>
+				<form onSubmit={this.handleModifyBlogSubmit}>
+					<p>author</p>
+					<input type='text' name='author' value={this.state.author} onChange={e => this.handleChange(e)}/>
+					<p>title</p>
+					<input type='text' name='title' value={this.state.title} onChange={e => this.handleChange(e)}/>
+					<p>content</p>
+					<textarea rows="4" cols="50" name='content' value={this.state.content} onChange={e => this.handleChange(e)}></textarea>
+					<button type='Submit'>Modify</button>
+				</form>
+				<footer></footer>
+			</page>
+    )
+  }
 
 	renderAbout() {
   	
@@ -130,26 +178,14 @@ class App extends Component {
 			return this.renderHome();
 		} else if (this.state.pageState === this.state.pageStates[1]){
 			return this.renderNewBlogPost();
+		} else if (this.state.pageState === this.state.pageStates[2]) {
+			return this.renderModifyBlogPost();
 		} else {
-			return this.renderAbout();
-		}
+      return this.renderAbout();
+    }
 	}
 
 	render() {
-		/*
-		return (
-			<page>
-				<ul>
-					<li><a href="">Home</a></li>
-					<li><a>Contact</a></li>
-					<li><a>About</a></li>
-					<li><a href="">Create New Blog Post</a></li>
-					<input type="text" placeholder="Search.."></input>
-				</ul>
-				<BlogPostList onDelete={this.onDelete} blogPosts={this.state.blogPosts}/>
-				<footer></footer>
-			</page>
-		)*/
 		return this.renderPage();
 	}
 }
