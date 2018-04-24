7b07cf83a153108d7b85154d19f7c86ca2ff45d9
Infinitz <ristopulkkinen94@gmail.com>
2018-04-24 12:28:33 +0300

    Add javadoc to NestblogApplication.java.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/NesteblogApplication.java
@@ -6,13 +6,30 @@ import org.springframework.context.annotation.Bean;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+/**
+ * Starts the service.
+ *
+ * @author penjami
+ * @version 1.0
+ * @since 1.0
+ */
 @SpringBootApplication
 public class NesteblogApplication {
 
+    /**
+     * Starts the service.
+     *
+     * @param args Possible command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(NesteblogApplication.class, args);
     }
 
+    /**
+     * Controls what is the main page.
+     *
+     * @return The main page.
+     */
     @Bean
     public ClassLoaderTemplateResolver reactAppTemplateResolver() {
         ClassLoaderTemplateResolver reactAppTemplateResolver = new ClassLoaderTemplateResolver();
@@ -25,6 +42,4 @@ public class NesteblogApplication {
 
         return reactAppTemplateResolver;
     }
-
-
 }
