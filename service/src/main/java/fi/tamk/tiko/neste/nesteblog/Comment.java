6cf33426113a1395b022d88b1b375b59e9a3d655
Infinitz <ristopulkkinen94@gmail.com>
2018-04-24 12:23:24 +0300

    Add javadoc to Comment.java.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/Comment.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/Comment.java
@@ -1,20 +1,40 @@
 package fi.tamk.tiko.neste.nesteblog;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
+/**
+ * This class contains all the information of the comments. They are embedded in the BlogPost class.
+ *
+ * @author penjami
+ * @version 1.0
+ * @since 1.0
+ */
 @Embeddable
 public class Comment {
 
+    /**
+     * Creator of the comment.
+     */
     private String author;
+
+    /**
+     * Content of the comment.
+     */
     private String content;
 
+    /**
+     * Default class constructor.
+     */
     public Comment() {
 
     }
 
+    /**
+     * Constructor which gets the author and content.
+     *
+     * @param author Creator of the comment.
+     * @param content Content of the comment.
+     */
     public Comment(String author, String content) {
         this.author = author;
         this.content = content;
