f23dceec296d6018dfe392be5a09016b3ecaff2f
Infinitz <ristopulkkinen94@gmail.com>
2018-04-24 12:21:00 +0300

    Add javadoc to CannotFindBlogPostException.java.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/CannotFindBlogPostException.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/CannotFindBlogPostException.java
@@ -1,13 +1,29 @@
 package fi.tamk.tiko.neste.nesteblog;
 
+/**
+ * Simple exception class to warn about BlogPosts.
+ *
+ * @author penjami
+ * @version 1.0
+ * @since 1.0
+ */
 public class CannotFindBlogPostException extends RuntimeException {
-    private long customerId;
+    /**
+     * ID of the BlogPost.
+     */
+    private long blogPostID;
 
+    /**
+     * @param customerID Saves this ID to the class variable.
+     */
     public CannotFindBlogPostException(Long customerID) {
-        this.customerId = customerID;
+        this.blogPostID = customerID;
     }
 
+    /**
+     * @return BlogPostID saved in this class.
+     */
     public long getCustomerId() {
-        return customerId;
+        return blogPostID;
     }
 }
