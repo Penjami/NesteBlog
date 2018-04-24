8c2a890464894c4ac3f35fdc6b9a8263593e9232
Infinitz <ristopulkkinen94@gmail.com>
2018-04-24 12:17:28 +0300

    Add javadoc to BlogRestController.java.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogRestController.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogRestController.java
@@ -8,17 +8,32 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
-
+/**
+ * Controller for handling database interactions.
+ *
+ * @author penjami
+ * @version 1.0
+ * @since 1.0
+ */
 @RestController
 public class BlogRestController {
 
+    /**
+     * BlogPostRepository is autowired to save an instance of itself in this class.
+     */
     @Autowired
     BlogPostRepository blogPostRepository;
 
+    /**
+     * Simple lorem ipsum String for testing the application.
+     */
     private String temp = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat orci auctor " +
             "mauris ornare, sit amet aliquam turpis semper. Vestibulum ante ipsum primis in faucibus orci luctus et " +
             "ultrices posuere cubilia Curae; Fusce feugiat arcu eget dictum elementum. Integer ultrices pretium posuere.";
 
+    /**
+     * After starting the service, add some test data to the database.
+     */
     @PostConstruct
     public void init() {
         BlogPost post = new BlogPost("Nancy Drake", temp, "jeeee");
@@ -36,16 +51,27 @@ public class BlogRestController {
         addBlogPost(post);
     }
 
+    /**
+     * @return All blogposts from the database.
+     */
     @RequestMapping(value = "/api/blogposts", method = RequestMethod.GET)
     public Iterable<BlogPost> getBlogPosts() {
         return blogPostRepository.findAll();
     }
 
+    /**
+     * @param blogPostID ID of the wanted BlogPost.
+     * @return BlogPost with the given ID if found.
+     */
     @RequestMapping(value = "/api/blogposts/{blogPostID}", method = RequestMethod.GET)
     public BlogPost getBlogPost(@PathVariable long blogPostID) {
         return blogPostRepository.findById(blogPostID).orElseThrow(() -> new CannotFindBlogPostException(blogPostID));
     }
 
+    /**
+     * @param blogPostID ID of the to be deleted BlogPost.
+     * @return Response according to what happened during deleting.
+     */
     @RequestMapping(value = "/api/blogposts/{blogPostID}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteBlogPost(@PathVariable long blogPostID) {
         BlogPost post = blogPostRepository.findById
@@ -54,6 +80,11 @@ public class BlogRestController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * @param blogPostID ID of the BlogPost to be updated.
+     * @param blogPostDetails Details of the BlogPost.
+     * @return Updated version of the BlogPost.
+     */
     @PostMapping("/api/blogposts/{id}")
     public BlogPost updateBlogPost(@PathVariable(value = "id") Long blogPostID,
                            @Valid @RequestBody BlogPost blogPostDetails) {
@@ -71,6 +102,10 @@ public class BlogRestController {
         return blogPostRepository.save(newBlogPost);
     }
 
+    /**
+     * @param blogPost BlogPost to be saved.
+     * @return Response according to what happened during the saving of the BlogPost.
+     */
     @PostMapping(value = "/api/blogposts/")
     public ResponseEntity<Void> addBlogPost(@RequestBody BlogPost blogPost) {
         blogPostRepository.save(blogPost);
