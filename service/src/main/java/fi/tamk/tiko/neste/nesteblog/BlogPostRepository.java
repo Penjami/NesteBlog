3a6d05a23b8e3013cd00be9d728e1d13755edeff
Infinitz <ristopulkkinen94@gmail.com>
2018-04-24 12:01:39 +0300

    Add javadoc to BlogPostReopsitory.java.

--- service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogPostRepository.java
+++ service/src/main/java/fi/tamk/tiko/neste/nesteblog/BlogPostRepository.java
@@ -2,5 +2,12 @@ package fi.tamk.tiko.neste.nesteblog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Helper interface for managing database by extending JpaRepository.
+ *
+ * @author penjami
+ * @version 1.0
+ * @since 1.0
+ */
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 }
